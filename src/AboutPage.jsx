dd29a6dde5f9bfdbecf76d1aec7dab80e99a3105
Wesley Dai <slipyslidymaple@gmail.com>
2018-11-17 21:06:37 -0600

    Reorganize site structure

---
+++ src/AboutPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+          <p>
+            Welcome to the MapleStory Music database. This site provides a complete listing of the background music (BGM)
+            used in MapleStory. Collectively, the songs are also known as MapleStory's original soundtrack (OST).
+          </p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
