b86e1109c910117d6154d7d99f92ec08493d12ad
Wesley Dai <slipyslidymaple@gmail.com>
2020-05-17 18:34:56 -0400

    Reorganize text on pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import { css, jsx } from '@emotion/core';
 import { Form, InputGroup } from 'react-bootstrap';
 import ReactPlayer from 'react-player';
@@ -31,22 +31,31 @@ const HomePage: React.FC = () => {
 
   return (
     <div>
-      {currentSong === undefined && (
-        <div>
-          <img
-            css={css`
-              display: block;
-              margin-left: auto;
-              margin-right: auto;
-              margin-bottom: 10px;
-            `}
-            id='header-logo'
-            src='assets/pink-bean.png'
-            alt='header logo'
-          />
-        </div>
-      )}
-      {currentSong !== undefined && (
+      {currentSong === undefined ? (
+        <Fragment>
+          <div>
+            <img
+              css={css`
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                margin-bottom: 10px;
+              `}
+              id='header-logo'
+              src='assets/pink-bean.png'
+              alt='header logo'
+            />
+          </div>
+          <div>
+            <p>
+              Welcome to the MapleStory Music database. This site provides a
+              complete listing of the background music (BGM) used in MapleStory.
+              Collectively, the songs are also known as MapleStory's original
+              soundtrack (OST).
+            </p>
+          </div>
+        </Fragment>
+      ) : (
         <div>
           <ReactPlayer
             css={css`
@@ -64,7 +73,7 @@ const HomePage: React.FC = () => {
       )}
       <Form.Group
         css={css`
-          margin: 2vh 14vw;
+          margin: 10px 14vw;
         `}
         className='filter-text'
       >
