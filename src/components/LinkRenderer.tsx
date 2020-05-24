a5b38819b8ef53f1f51f7365a76694c9028b9005
Wesley Dai <slipyslidymaple@gmail.com>
2020-05-23 20:58:05 -0400

    Enhance external link tooltip

--- src/components/LinkRenderer.tsx
+++ src/components/LinkRenderer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { css, jsx } from '@emotion/core';
 import { ICellRendererParams } from 'ag-grid-community';
 import ReactGA from 'react-ga';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 interface ILinkRenderer extends ICellRendererParams {
   title: string;
@@ -30,6 +31,9 @@ const LinkRenderer: React.FC<ILinkRenderer> = ({
       action: 'View external video',
       label: youtube,
     });
+    // Handle tooltip defocus
+    const activeElement = document.activeElement as HTMLElement;
+    activeElement.blur();
   };
   return (
     <div>
@@ -37,19 +41,26 @@ const LinkRenderer: React.FC<ILinkRenderer> = ({
         <span>{title}</span>
       ) : (
         <span>
-          <a
-            css={css`
-              color: inherit;
-              margin-right: 3px;
-            `}
-            href={`https://youtu.be/${youtube}`}
-            rel='noopener noreferrer'
-            target='_blank'
-            title='View on YouTube'
-            onClick={onExternalClick}
+          <OverlayTrigger
+            placement={'top'}
+            delay={{ show: 250, hide: 100 }}
+            overlay={
+              <Tooltip id={`tooltip-external-link`}>View on YouTube</Tooltip>
+            }
           >
-            <i className='fa fa-external-link'></i>
-          </a>
+            <a
+              css={css`
+                color: inherit;
+                margin-right: 3px;
+              `}
+              href={`https://youtu.be/${youtube}`}
+              rel='noopener noreferrer'
+              target='_blank'
+              onClick={onExternalClick}
+            >
+              <i className='fa fa-external-link'></i>
+            </a>
+          </OverlayTrigger>
           <a href='# ' rel='noopener noreferrer' onClick={onEmbeddedClick}>
             {title}
           </a>
