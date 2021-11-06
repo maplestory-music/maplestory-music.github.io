d2419fcee74293cf7de392f28d81a12babfe28a6
Wesley Dai <slipyslidymaple@gmail.com>
2021-11-06 13:50:36 -0500

    Chart cumulative song count for monthly frequency stats

--- src/components/charts/MonthlyFrequencyChart.tsx
+++ src/components/charts/MonthlyFrequencyChart.tsx
@@ -10,12 +10,20 @@ import { ICommonChartProps } from './CommonChartProps';
 const MonthlyFrequencyChart: React.FC<ICommonChartProps> = (props) => {
   const dataSource: IMusicRecordGrid[] = useDataSourceState();
   const selectedYear = props.selectedYear;
-  const monthlyFreq = dataSource.reduce((prev: Array<number>, current) => {
+  const monthlyFreq = dataSource.reduce((prev: number[], current) => {
     if (
       current.metadata.year === selectedYear.toString() &&
       current.source.date
     ) {
       prev[getMonth(current.source.date)]++;
+    }
+    return prev;
+  }, new Array(12).fill(0));
+  const cumulativeCount = monthlyFreq.reduce((prev: number[], current, idx) => {
+    if (idx === 0) {
+      prev[0] = current;
+    } else {
+      prev[idx] = prev[idx - 1] + current;
     }
     return prev;
   }, new Array(12).fill(0));
@@ -30,18 +38,35 @@ const MonthlyFrequencyChart: React.FC<ICommonChartProps> = (props) => {
     xAxis: {
       type: 'datetime',
     },
-    yAxis: {
-      title: {
-        text: 'Songs',
+    yAxis: [
+      {
+        title: {
+          text: 'Songs',
+        },
       },
-    },
+      {
+        title: {
+          text: 'Cumulative',
+        },
+        opposite: true,
+      },
+    ],
     series: [
       {
         name: 'Songs',
         type: 'line',
+        yAxis: 0,
         pointStart: Date.UTC(selectedYear, 0, 1),
         pointIntervalUnit: 'month',
         data: monthlyFreq,
+      },
+      {
+        name: 'Cumulative',
+        type: 'line',
+        yAxis: 1,
+        pointStart: Date.UTC(selectedYear, 0, 1),
+        pointIntervalUnit: 'month',
+        data: cumulativeCount,
       },
     ],
   };
