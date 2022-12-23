780eb3f6bb6c98ce657769e83f0fcaf6ccc972e4
Wesley Dai <slipyslidymaple@gmail.com>
2022-12-23 18:47:10 -0500

    Set up GitHub Actions

--- env.sh
+++ env.sh
@@ -5,6 +5,8 @@ if [ -z "${GA_TOKEN}" ]; then
 fi
 BUILD_HASH="$(git rev-parse --short HEAD)"
 BUILD_DATE="$(date -I)"
+echo $BUILD_HASH
+echo $BUILD_DATE
 echo "REACT_APP_GA_TOKEN=${GA_TOKEN}" > ./.env.production.local
 echo "REACT_APP_BUILD_HASH=${BUILD_HASH}" >> ./.env.production.local
 echo "REACT_APP_BUILD_DATE=${BUILD_DATE}" >> ./.env.production.local
